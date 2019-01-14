62db40a242b793a56b1a17d4df81d65bda7b4910
petrosFr <subhiissa@gmail.com>
2019-01-14 15:24:43 +0100

    bug correction

--- LOD-CM-WEB/Program.cs
+++ LOD-CM-WEB/Program.cs
@@ -21,17 +21,22 @@ namespace LOD_CM
         public static string localGraphvizDotPath { get; private set; }
         public static void Main(string[] args)
         {
-            if (args.Length < 3)
-                throw new ArgumentException("You must provide a valid main directory, the PlantUml jar path and the Graphviz Dot path. (in that order)");
+            if (args.Length < 1)
+                throw new ArgumentException("You must provide a valid main directory.");
             mainDir = args[0];
-            plantUmlJarPath = args[1];
-            localGraphvizDotPath = args[2];
             if (!Directory.Exists(mainDir))
                 throw new DirectoryNotFoundException($"{mainDir} doesn't exist!");
-            if (!File.Exists(plantUmlJarPath))
-                throw new FileNotFoundException($"{plantUmlJarPath} doesn't exist!");
-            if (!File.Exists(localGraphvizDotPath))
-                throw new FileNotFoundException($"{localGraphvizDotPath} doesn't exist!");
+            // if (args.Length < 3)
+            //     throw new ArgumentException("You must provide a valid main directory, the PlantUml jar path and the Graphviz Dot path. (in that order)");
+            // mainDir = args[0];
+            // plantUmlJarPath = args[1];
+            // localGraphvizDotPath = args[2];
+            // if (!Directory.Exists(mainDir))
+            //     throw new DirectoryNotFoundException($"{mainDir} doesn't exist!");
+            // if (!File.Exists(plantUmlJarPath))
+            //     throw new FileNotFoundException($"{plantUmlJarPath} doesn't exist!");
+            // if (!File.Exists(localGraphvizDotPath))
+            //     throw new FileNotFoundException($"{localGraphvizDotPath} doesn't exist!");
             CreateWebHostBuilder(args).Build().Run();
         }
 
